ef2c0ede716267e9f95c73e7c7da16d44fb59f6e
Sebastian Schmidt <isibboi@gmail.com>
2018-10-25 22:38:29 +0200

    Removed velocity dependence of animation.
    Animation works based on player input now.

--- src/systems/animation.rs
+++ src/systems/animation.rs
@@ -1,5 +1,5 @@
 use amethyst::ecs::{System, Join, Read, ReadStorage, WriteStorage};
-use components::{Animated, Velocity, WorldCollisionAffected};
+use components::{Animated, WorldCollisionAffected};
 use amethyst::input::InputHandler;
 use amethyst::renderer::SpriteRender;
 use amethyst::core::Time;
@@ -9,10 +9,10 @@ const ANIMATION_DELAY: f32 = 0.2;
 pub struct AnimationSystem;
 
 impl<'s> System<'s> for AnimationSystem {
-    type SystemData = (WriteStorage<'s, Animated>, ReadStorage<'s, Velocity>, ReadStorage<'s, WorldCollisionAffected>, WriteStorage<'s, SpriteRender>, Read<'s, Time>, Read<'s, InputHandler<String, String>>);
+    type SystemData = (WriteStorage<'s, Animated>, ReadStorage<'s, WorldCollisionAffected>, WriteStorage<'s, SpriteRender>, Read<'s, Time>, Read<'s, InputHandler<String, String>>);
 
-    fn run(&mut self, (mut animateds, velocities, world_collision_affecteds, mut sprite_renders, time, input_handler): <Self as System<'s>>::SystemData) {
-        for (mut animated, velocity, world_collision_affected, mut sprite_render) in (&mut animateds, &velocities, &world_collision_affecteds, &mut sprite_renders).join() {
+    fn run(&mut self, (mut animateds, world_collision_affecteds, mut sprite_renders, time, input_handler): <Self as System<'s>>::SystemData) {
+        for (mut animated, world_collision_affected, mut sprite_render) in (&mut animateds, &world_collision_affecteds, &mut sprite_renders).join() {
             let move_axis = input_handler.axis_value("move").unwrap();
             if !world_collision_affected.on_ground {
                 sprite_render.sprite_number = 1;
