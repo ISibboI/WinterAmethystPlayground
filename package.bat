a3f71190b407e7110edd300a6de6944669f44336
Sebastian Schmidt <isibboi@gmail.com>
2018-11-06 22:05:49 +0100

    Added windows packaging script

---
+++ package.bat
@@ -0,0 +1,9 @@
+title Packaging the game
+
+git pull
+cargo update
+cargo +nightly build --release
+copy /b target\release\test_game.exe test_game.exe
+del merry_christmas.rar
+"C:\Program Files\WinRAR\Rar.exe" a merry_christmas.rar test_game.exe resources
+del test_game.exe
