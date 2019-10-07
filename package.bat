c0b8301e76697eed19c90b61a6c2f2158afb60b9
Sebastian Schmidt <isibboi@gmail.com>
2019-10-07 18:27:39 +0200

    Rename resources directory to assets in package scripts

--- package.bat
+++ package.bat
@@ -5,6 +5,6 @@ cargo update
 cargo +nightly build --release
 copy /b target\release\test_game.exe test_game.exe
 del merry_christmas.rar
-"C:\Program Files\WinRAR\Rar.exe" a merry_christmas.rar test_game.exe resources
+"C:\Program Files\WinRAR\Rar.exe" a merry_christmas.rar test_game.exe assets
 del test_game.exe
 pause
--- package.sh
+++ package.sh
@@ -4,5 +4,5 @@ set -e
 
 cargo +nightly build --release
 cp target/release/test_game .
-tar -caf merry_christmas.tar.gz test_game resources
+tar -caf merry_christmas.tar.gz test_game assets
 rm test_game
